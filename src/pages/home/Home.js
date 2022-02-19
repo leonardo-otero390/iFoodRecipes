a2cfb96a5bac4e08daa12a4b2c084051153d1bc6
Leonardo Otero <leonardo.otero390@gmail.com>
2022-02-19 14:34:21 -0400

    feat: Input layout

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,11 @@
+import Input from "../../components/Input";
 import TopBar from "../../components/TopBar";
 
 export default function Home() {
   return (
     <>
       <TopBar content="RECEITAS IFOOD" />
-      <h1> Bem vindo(a) ao ifood recipes</h1>
+      <Input />
     </>
   );
 }
