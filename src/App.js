0d13d4ada44e381120b4c519297e395c7b0e816e
Leonardo Otero <leonardo.otero390@gmail.com>
2022-02-19 12:36:36 -0400

    feat: set index.html and global styles

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import GlobalStyle from "./styles/global";
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h1> Bem vindo(a) ao ifood recipes</h1>
+    </>
+  );
+}
